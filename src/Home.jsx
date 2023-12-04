d4a676336628f3c6fbf0eadf92680dabb9eec81b
Tom Serenander <tom@serenander.se>
2023-12-04 14:37:43 +0100

    Orange button.

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,11 @@ export default function Home() {
 		<div className="home">
 			<div className="container">
 				<h1>You got the travel plans, we got the travel vans.</h1>
+				<p>
+					Add adventure to your life by joining the #vanlife movement.
+					Rent the perfect van to make your perfect road trip.
+				</p>
+				<button className="big-btn">Find your van</button>
 			</div>
 		</div>
 	)
