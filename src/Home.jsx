b5e887d97d1fd4a9a4e3de684f45e1edd65387aa
Tom Serenander <tom@serenander.se>
2023-12-04 13:32:18 +0100

    Home page, first draft.

---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className="home">
+			<div className="container">
+				<h1>You got the travel plans, we got the travel vans.</h1>
+			</div>
+		</div>
+	)
+}
--- src/index.jsx
+++ src/index.jsx
@@ -2,29 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './index.css'
+import Home from './Home'
 
 function App() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter className="app">
 			<nav>
-				<Link to="/">Home</Link>
-				<Link to="/about">About</Link>
+				<div className="container">
+					<Link className="logo-text" to="/">
+						#VANLIFE
+					</Link>
+					<Link to="/about">About</Link>
+					<Link to="/vans">Vans</Link>
+				</div>
 			</nav>
 
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
+				<Route path="/vans" element={<Vans />} />
 			</Routes>
+
+			<footer>
+				<p>Ⓒ 2023 #VANLIFE</p>
+			</footer>
 		</BrowserRouter>
 	)
 }
 
-function Home() {
-	return <h1>Hello, React Router!</h1>
-}
-
 function About() {
 	return <h1>About page goes here! 🎉</h1>
+}
+function Vans() {
+	return <h1>Vans page goes here! 🎉</h1>
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
