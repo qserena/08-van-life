56e209a600494aac5496fd34d01bda4d1b93412b
Tom Serenander <tom@serenander.se>
2023-12-04 12:00:46 +0100

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import './index.css'
+
+function App() {
+	return (
+		<BrowserRouter>
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/about">About</Link>
+			</nav>
+
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</BrowserRouter>
+	)
+}
+
+function Home() {
+	return <h1>Hello, React Router!</h1>
+}
+
+function About() {
+	return <h1>About page goes here! 🎉</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
