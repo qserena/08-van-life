246cbac0faa8a0a7e48c2c0c6e80cf413be319e8
Tom Serenander <tom@serenander.se>
2023-12-05 18:55:21 +0100

    Moved Link tag to get rid of underline.

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -12,8 +12,8 @@ export default function Vans() {
 	}, [])
 
 	const vanElements = vans.map((van) => (
-		<Link to={`/vans/${van.id}`}>
-			<div key={van.id} className="van-tile">
+		<div key={van.id} className="van-tile">
+			<Link to={`/vans/${van.id}`}>
 				<img src={van.imageUrl} />
 				<div className="van-info">
 					<h3>{van.name}</h3>
@@ -23,8 +23,8 @@ export default function Vans() {
 					</p>
 				</div>
 				<i className={`van-type ${van.type} selected`}>{van.type}</i>
-			</div>
-		</Link>
+			</Link>
+		</div>
 	))
 
 	return (
