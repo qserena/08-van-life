68a9852e49ef89637315324bbe252af8db50775f
Tom Serenander <tom@serenander.se>
2023-12-06 16:45:50 +0100

    Populated HostVans list.

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,3 +1,31 @@
+import { useState, useEffect } from 'react'
+
 export default function HostVans() {
-	return <h1>Here is Vans!</h1>
+	const [vans, setVans] = useState([])
+
+	useEffect(() => {
+		fetch('/api/host/vans')
+			.then((response) => response.json())
+			.then((data) => setVans(data.vans))
+	}, [])
+
+	const vanElements = vans.map((van) => (
+		<div className="host-vans-card">
+			<img src={van.imageUrl} alt="photo of van" />
+			<div className="host-vans-card-info">
+				<h3>{van.name}</h3>
+				<p>
+					${van.price}
+					<span>/day</span>
+				</p>
+			</div>
+		</div>
+	))
+
+	return (
+		<div className="host-van-list-container">
+			<h2>Your listed vans</h2>
+			{vanElements}
+		</div>
+	)
 }
