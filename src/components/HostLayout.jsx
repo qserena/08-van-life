109b1bfd90cf68a5e84e12330a6b92e2bc748f5d
Tom Serenander <tom@serenander.se>
2023-12-06 11:28:11 +0100

    Switched to relative route paths.

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -4,7 +4,7 @@ export default function HostLayout() {
 	return (
 		<>
 			<nav className="host-nav">
-				<Link to="/host/dashboard">Dashboard</Link>
+				<Link to="/host">Dashboard</Link>
 				<Link to="/host/income">Income</Link>
 				<Link to="/host/reviews">Reviews</Link>
 			</nav>
--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,7 +1,3 @@
 export default function Dashboard() {
-	return (
-		<>
-			<h1>Dash</h1>
-		</>
-	)
+	return <h1>Dash</h1>
 }
