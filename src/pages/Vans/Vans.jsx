6bd06b24513225c94c6e2456f839748cf2af2164
Tom Serenander <tom@serenander.se>
2023-12-07 16:40:12 +0100

    Implemented 'selected' on filter buttons.

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -34,28 +34,46 @@ export default function Vans() {
 		</div>
 	))
 
+	function handleFilterChange(key, value) {
+		setSearchParams((prevParams) => {
+			if (value === null) {
+				prevParams.delete(key)
+			} else {
+				prevParams.set(key, value)
+			}
+			return prevParams
+		})
+	}
+
 	return (
 		<div className="van-list-container">
 			<h2>Explore our van options</h2>
 			<div className="van-list-filter-buttons">
 				<button
-					onClick={() => setSearchParams({ type: 'simple' })}
-					className="van-type simple"
+					onClick={() => handleFilterChange('type', 'simple')}
+					className={`van-type simple ${
+						typeFilter === 'simple' ? 'selected' : ''
+					}`}
 				>
 					Simple
 				</button>
 				<button
-					onClick={() => setSearchParams({ type: 'luxury' })}
-					className="van-type luxury"
+					onClick={() => handleFilterChange('type', 'luxury')}
+					className={`van-type luxury ${
+						typeFilter === 'luxury' ? 'selected' : ''
+					}`}
 				>
 					Luxury
 				</button>
 				<button
-					onClick={() => setSearchParams({ type: 'rugged' })}
-					className="van-type rugged"
+					onClick={() => handleFilterChange('type', 'rugged')}
+					className={`van-type rugged ${
+						typeFilter === 'rugged' ? 'selected' : ''
+					}`}
 				>
 					Rugged
 				</button>
+
 				{typeFilter && (
 					<button
 						onClick={() => setSearchParams({})}
