a11b11820ab4f7e042efc88842c3793f21f1c5a6
Tom Serenander <tom@serenander.se>
2023-12-07 11:41:40 +0100

    HostVanDetails back button destination.

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -21,7 +21,7 @@ export default function HostVanDetail() {
 
 	return (
 		<section>
-			<Link to="?" className="back-button">
+			<Link to=".." relative="path" className="back-button">
 				&larr; <span>Back to all vans</span>
 			</Link>
 
