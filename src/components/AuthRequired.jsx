8cbd080ede8927e2100d61e63acce638be99873b
Tom Serenander <tom@serenander.se>
2023-12-09 08:32:18 +0100

    'You must log in first'.

--- src/components/AuthRequired.jsx
+++ src/components/AuthRequired.jsx
@@ -1,10 +1,15 @@
 import { Navigate, Outlet } from 'react-router-dom'
 
 export default function AuthRequired() {
-	const authenticated = false
+	const authenticated = true
 
 	if (!authenticated) {
-		return <Navigate to="/login" />
+		return (
+			<Navigate
+				to="/login"
+				state={{ message: 'You must log in first' }}
+			/>
+		)
 	}
 
 	return <Outlet />
